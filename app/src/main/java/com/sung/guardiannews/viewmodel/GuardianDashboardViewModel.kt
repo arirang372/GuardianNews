7b4d65fdc72e5076a861b430f5b73dce5365dba3
arirang372 <hyonoo419@gmail.com>
2021-12-21 00:26:05 -0500

    Parellel webservice calls

--- app/src/main/java/com/sung/guardiannews/viewmodel/GuardianDashboardViewModel.kt
+++ app/src/main/java/com/sung/guardiannews/viewmodel/GuardianDashboardViewModel.kt
@@ -1,5 +1,6 @@
 package com.sung.guardiannews.viewmodel
 
+import android.util.Log
 import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
@@ -10,11 +11,11 @@ import com.sung.guardiannews.data.GuardianNewsRepository
 import com.sung.guardiannews.data.remote.GuardianServiceResponseResult
 import com.sung.guardiannews.model.Section
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.system.measureTimeMillis
 
 @HiltViewModel
 class GuardianDashboardViewModel @Inject constructor(
@@ -43,27 +44,17 @@ class GuardianDashboardViewModel @Inject constructor(
         getSections()
     }
 
-    private fun getSections() = viewModelScope.launch(Dispatchers.IO) {
+    private fun getSections() = viewModelScope.launch {
         dataLoading.set(true)
         try {
-            val response = repository.getSections().response
-            coroutineScope {
-                response.results.forEach {
-                    try {
-                        it.articles =
-                            withContext(Dispatchers.Default) {
-                                it.sectionName?.let { sectionId ->
-                                    repository.getArticles(
-                                        sectionId
-                                    ).data
-                                }
-                            }!!
-                    } catch (exception: Exception) {
-                        //do nothing...
-                    }
-                }
+            val elapsed = measureTimeMillis {
+                val response = repository.getSections().response
+                response.results.map {
+                    async { processResult(it) }
+                }.awaitAll()
                 sectionResponseResult.postValue(GuardianServiceResponseResult.success(response.results))
             }
+            Log.v("Sections", "it took $elapsed ms")
         } catch (exception: Exception) {
             sectionResponseResult.postValue(
                 GuardianServiceResponseResult.error(exception.toString(), null)
@@ -73,37 +64,14 @@ class GuardianDashboardViewModel @Inject constructor(
         }
     }
 
-    /**
-     *  Trying to handle it through Flow
-     *
-     */
-//    private fun getSections() = viewModelScope.launch(Dispatchers.IO) {
-//        dataLoading.set(true)
-//        try {
-//            var response = repository.getSections().response
-//            var sections = response.results
-//            runBlocking {
-//                val deferreds: List<Deferred<Unit>> = sections.map {
-//                    async {
-//                        delay(500)
-//                        it.articles = it.sectionName?.let { it1 -> getSectionArticles(it1) }
-//                    }
-//                }
-//                deferreds.awaitAll()
-//                sectionResponseResult.postValue(GuardianServiceResponseResult.success(sections))
-//            }
-//        } catch (exception: Exception) {
-//            sectionResponseResult.postValue(
-//                GuardianServiceResponseResult.error(exception.toString(), null)
-//            )
-//        } finally {
-//            dataLoading.set(false)
-//        }
-//    }
-
-    private suspend fun getSectionArticles(sectionId: String) =
-        repository.getListArticles(sectionId)
-
+    private suspend fun processResult(section: Section): Section {
+        try {
+            section.articles = section.sectionName?.let { it -> repository.getArticles(it).data }
+        } catch (exception: Exception) {
+            //do nothing...
+        }
+        return section
+    }
 
     fun getSectionResponseResult(): LiveData<GuardianServiceResponseResult<List<Section>>> =
         sectionResponseResult
